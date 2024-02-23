7f40d4046e9cc2217085d11341f212e7aef94cdb
Álvaro <Alvaromendoza01@hotmail.com>
2024-02-22 21:16:03 -0300

    AHORA SI

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ inputBox.addEventListener('keydown',function(e) {
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span)
-    
+        inputBox.value = "";
         }}
-    inputBox.value = "";
+
  }
 )
